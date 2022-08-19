0eee46ecd4a8988f97b740f969f9fc6f24514df6
Pranjal-Tibrewal <TibrewalPranjal@JohnDeere.com>
2022-08-19 12:46:23 +0530

    first commit

---
+++ firstbash.java
@@ -0,0 +1,7 @@
+class Firstbash{
+    public static void main(String args){
+        System.out.println("hello World...git demo");
+        System.out.println("some changes in the file- gti demo");
+        System.out.println("we are making new changes");
+    }
+}
